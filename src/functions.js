1f71297538ae8df2300810dfee8b0730965832c0
ben <ben.pamittan@hotmail.com>
2024-04-07 23:15:01 +1000

    functioning insight just need to clean up.

--- src/functions.js
+++ src/functions.js
@@ -1,21 +1,69 @@
 
 
 
-function createChart(ctx, inputData) {
+function createChart(ctx, inputLabels, inputData) {
+
+    let House = {
+      label: 'House',
+      data: inputData[0],
+      borderWidth: 1
+    }
+
+    let Popping = {
+      label: 'Popping',
+      data: inputData[1],
+      borderWidth: 1
+    }
+
+    let Breaking = {
+      label: 'Breaking',
+      data: inputData[2],
+      borderWidth: 1
+    }
+
+    let HipHop = {
+      label: 'Hip Hop',
+      data: inputData[3],
+      borderWidth: 1
+    }
+
+    let Krump = {
+      label: 'Krump',
+      data: inputData[4],
+      borderWidth: 1
+    }
+
+    let FreeStyle = {
+      label: 'Freestyle Team',
+      data: inputData[5],
+      borderWidth: 1
+    }
+
+
+    let dataSets = [House, Popping, Breaking, HipHop, Krump, FreeStyle];
+
+
+
     new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-            label: '# of Votes',
-            data: inputData,
-            borderWidth: 1
-          }]
+          labels: inputLabels,
+          datasets: dataSets
         },
         options: {
           scales: {
+            x: {
+              title: {
+                display: true,
+                text: 'Week'
+              }
+            },
             y: {
-              beginAtZero: true
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Number of Students'
+              }
             }
           }
         }
--- src/server.js
+++ src/server.js
@@ -6,6 +6,23 @@ const { google } = require("googleapis");
 
 const app = express();
 
+//FUNCTION DECLARATIONS
+
+//function for formatting date object
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${day}/${month}/${year}`;
+}
+
+//function for getting the date for the start of the week given a date
+function getStartOfWeek(date) {
+    const weekStart = date;
+    weekStart.setDate(weekStart.getDate() - weekStart.getDay() + (weekStart.getDay() === 0 ? -6 : 1));
+    return formatDate(weekStart);
+}
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -36,7 +53,7 @@ app.get("/db", async (req, res) => {
 
     //define authentication object
     const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
+        keyFile: "../credentials.json",
         scopes: "https://www.googleapis.com/auth/spreadsheets"
     });
 
@@ -49,10 +66,10 @@ app.get("/db", async (req, res) => {
     // get metadata about spreadsheet
     const spreadsheetId = "1rbyNBeNzD9D4RJ0r0Ojpo0QlRLaudQ8t3c6CjuNKaMo";
 
-    const metaData = await googleSheets.spreadsheets.get({
-        auth,
-        spreadsheetId
-    });
+    // const metaData = await googleSheets.spreadsheets.get({
+    //     auth,
+    //     spreadsheetId
+    // });
 
     //read rows from spreadsheet
 
@@ -62,7 +79,115 @@ app.get("/db", async (req, res) => {
         range: "Sheet1"
     })
 
-    res.send(getRows.data.values);
+    //manipulate the data to send to the website
+
+    let dataSize = getRows.data.values.length;
+    
+    //count how many attendees with
+    
+    const attendance = {};
+    // const classes = [];
+
+    dates = [];
+    weeks = [];
+    const nHouse = [];
+    const nPopping = [];
+    const nBreaking = [];
+    const nHipHop = [];
+    const nKrump = [];
+    const nFreeStyle = [];
+
+    //change date to week in attendace dictionary key
+    //and then change the update function
+
+    //count how many people came to each class
+    for (let i = 1; i < dataSize; i++) {
+        //get date
+        let date = getRows.data.values[i][0].split(" ")[0];
+        let dateSplit = date.split("/");
+
+        //get start of week
+        const dateObject = new Date(`${dateSplit[2]}-${dateSplit[0]}-${dateSplit[1]}`);
+        week = getStartOfWeek(dateObject);
+        console.log(dateSplit);
+        
+        //get class type
+        let classType = getRows.data.values[i][1];
+        //create attendance key by combining date and classType into a single string
+        attendanceKey = week + "-" + classType;
+
+        if (!weeks.includes(week)) {
+            //for the array to store the weeks in order
+            weeks.push(week);
+        }
+
+        
+        if (!dates.includes(date)) {
+            //for the array to store the dates in order
+            dates.push(date);
+        }
+
+        if (attendance.hasOwnProperty(attendanceKey)) {
+            //increase count if class already exists in dictionary
+            attendance[attendanceKey] += 1;
+        } else {
+            // //add new class to the classes array if class is not in the dictionary
+            // classes.push([attendanceKey]);
+            //add class to the dictionary
+            attendance[attendanceKey] = 1;
+        }
+    }
+
+    let weeksSize = weeks.length;
+
+    for (let i = 0; i < weeksSize; i++) {
+        //for each date update the nHouse, nPopping, nBreaking... arrays
+
+        let week = weeks[i]; 
+        
+        if (attendance.hasOwnProperty(week + "-" + "House")) {
+            nHouse.push(attendance[week + "-" + "House"]);
+        } else {
+            nHouse.push(0);
+        }
+
+        if (attendance.hasOwnProperty(week + "-" + "Breaking")) {
+            nBreaking.push(attendance[week + "-" + "Breaking"]);
+        } else {
+            nBreaking.push(0);
+        }
+
+        if (attendance.hasOwnProperty(week + "-" + "Hip Hop")) {
+            nHipHop.push(attendance[week + "-" + "Hip Hop"]);
+        } else {
+            nHipHop.push(0);
+        }
+
+        if (attendance.hasOwnProperty(week + "-" + "Popping")) {
+            nPopping.push(attendance[week + "-" + "Popping"]);
+        } else {
+            nPopping.push(0);
+        }
+
+        if (attendance.hasOwnProperty(week + "-" + "Krump")) {
+            nKrump.push(attendance[week + "-" + "Krump"]);
+        } else {
+            nKrump.push(0);
+        }
+
+        if (attendance.hasOwnProperty(week + "-" + "Freestyle Team")) {
+            nFreeStyle.push(attendance[week + "-" + "Freestyle Team"]);
+        } else {
+            nFreeStyle.push(0);
+        }
+    }
+
+    const ChartData = {
+        labels : weeks,
+        inputData : [nHouse, nPopping, nBreaking, nHipHop, nKrump, nFreeStyle]
+    }
+
+    res.send(ChartData);
 });
 
 
