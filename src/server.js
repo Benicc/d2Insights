0c47f4bf935b860440bb42e60cea3fc3cc341bef
ben <ben.pamittan@hotmail.com>
2024-04-07 23:29:24 +1000

    cleaning up redundant code

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ function getStartOfWeek(date) {
     return formatDate(weekStart);
 }
 
-
+//SERVICE GET REQUESTS
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -32,23 +32,8 @@ app.get('/functions', (req, res) => {
     res.sendFile(path.join(__dirname, 'functions.js'));
 });
 
-app.get('/users', (req, res) => {
-    const users = [{
-        id: "123",
-        name: "Shaun",
-    }, {
-        id: "234",
-        name: "Bob"
-    }, {
-        id: "345",
-        name: "Sue"
-    }]
-
-    res.json(users);
-});
-
-//get google sheet data
-//"https://www.googleapis.com/auth/spreadsheets"
+//get google sheet data and send it to the chart
+//so that it can read.
 app.get("/db", async (req, res) => {
 
     //define authentication object
@@ -66,29 +51,19 @@ app.get("/db", async (req, res) => {
     // get metadata about spreadsheet
     const spreadsheetId = "1rbyNBeNzD9D4RJ0r0Ojpo0QlRLaudQ8t3c6CjuNKaMo";
 
-    // const metaData = await googleSheets.spreadsheets.get({
-    //     auth,
-    //     spreadsheetId
-    // });
-
     //read rows from spreadsheet
-
     const getRows = await googleSheets.spreadsheets.values.get({
         auth,
         spreadsheetId,
         range: "Sheet1"
     })
 
+    ////////////////////////////////////////////////////////////
     //manipulate the data to send to the website
-
+    ///////////////////////////////////////////////////////////
     let dataSize = getRows.data.values.length;
-    
-    //count how many attendees with
-    
     const attendance = {};
-    // const classes = [];
 
-    dates = [];
     weeks = [];
     const nHouse = [];
     const nPopping = [];
@@ -97,10 +72,7 @@ app.get("/db", async (req, res) => {
     const nKrump = [];
     const nFreeStyle = [];
 
-    //change date to week in attendace dictionary key
-    //and then change the update function
-
-    //count how many people came to each class
+    //count how many people came to each class per week and for which class
     for (let i = 1; i < dataSize; i++) {
         //get date
         let date = getRows.data.values[i][0].split(" ")[0];
@@ -109,40 +81,31 @@ app.get("/db", async (req, res) => {
         //get start of week
         const dateObject = new Date(`${dateSplit[2]}-${dateSplit[0]}-${dateSplit[1]}`);
         week = getStartOfWeek(dateObject);
-        console.log(dateSplit);
         
         //get class type
         let classType = getRows.data.values[i][1];
-        //create attendance key by combining date and classType into a single string
+        //create attendance key by combining week and classType into a single string
         attendanceKey = week + "-" + classType;
 
         if (!weeks.includes(week)) {
-            //for the array to store the weeks in order
+            //for the weeks array to store all the weeks in order
             weeks.push(week);
         }
 
-        
-        if (!dates.includes(date)) {
-            //for the array to store the dates in order
-            dates.push(date);
-        }
-
         if (attendance.hasOwnProperty(attendanceKey)) {
-            //increase count if class already exists in dictionary
+            //increase count if week-class key already exists in dictionary
             attendance[attendanceKey] += 1;
         } else {
-            // //add new class to the classes array if class is not in the dictionary
-            // classes.push([attendanceKey]);
-            //add class to the dictionary
+            //we have encountered a new week-class combination
+            //add week-class key to the dictionary if its not already in the dictionary
             attendance[attendanceKey] = 1;
         }
     }
 
+    //update and form the res data structure
     let weeksSize = weeks.length;
-
     for (let i = 0; i < weeksSize; i++) {
-        //for each date update the nHouse, nPopping, nBreaking... arrays
-
+        //for each week add the respective values to the nHouse, nPopping, nBreaking... arrays
         let week = weeks[i]; 
         
         if (attendance.hasOwnProperty(week + "-" + "House")) {
@@ -182,6 +145,7 @@ app.get("/db", async (req, res) => {
         }
     }
 
+    //data struct to send
     const ChartData = {
         labels : weeks,
         inputData : [nHouse, nPopping, nBreaking, nHipHop, nKrump, nFreeStyle]
@@ -189,14 +153,6 @@ app.get("/db", async (req, res) => {
 
     res.send(ChartData);
 });
-
-
-app.get('/dataSet', (req, res) => {
-    const users = {values: [5, 19, 3, 5, 2, 3]};
-
-    res.json(users);
-
-})
 
 app.listen(8080, 'localhost', () => {
     console.log('Server is listening on port 8080');
